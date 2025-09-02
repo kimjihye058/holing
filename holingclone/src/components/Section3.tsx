009e3669a651975414ea1c79c5141770da98ef73
kimjihye058 <s2321@e-mirim.hs.kr>
2025-09-02 17:36:58 +0900

    #6 동영상 강의 부분 구현(컴포넌트 스타일 구현)

--- holingclone/src/components/Section3.tsx
+++ holingclone/src/components/Section3.tsx
@@ -13,7 +13,59 @@ function Section3() {
           습관과 자신감을 길러줘요
         </Span2>
       </SpansDiv>
-      <div></div>
+      <Steps>
+        <Step1>
+          <Photos
+            style={{ backgroundImage: "url('/assets/images/steps/step1.png')" }}
+          >
+            <img
+              src="/assets/images/steps/step1_beaver.svg"
+              alt="step1비버"
+              style={{
+                position: "absolute",
+                left: "-55px",
+                bottom: "-22px",
+              }}
+            />
+            <img
+              src="/assets/images/steps/step1_overlay.svg"
+              alt="step1오버레이"
+              width={264}
+              style={{
+                position: "absolute",
+                right: "-19px",
+                bottom: "96.08px",
+              }}
+            />
+          </Photos>
+          <Descs>
+            <StepNumber>
+              <StepNumberSpan>1</StepNumberSpan>
+            </StepNumber>
+            <Desc1>
+              <Desc1_1>문제풀이를 도와주는</Desc1_1>
+              <Desc1_2>동영상 강의</Desc1_2>
+            </Desc1>
+            <Desc2>어려운 문제가 있으면 동영상 강의를 듣고 기초 개념과 유형을 다시 학습해 보세요.</Desc2>
+          </Descs>
+        </Step1>
+        <Step2>
+          <Photos></Photos>
+          <Descs></Descs>
+        </Step2>
+        <Step1>
+          <Photos></Photos>
+          <Descs></Descs>
+        </Step1>
+        <Step2>
+          <Photos></Photos>
+          <Descs></Descs>
+        </Step2>
+        <Step1>
+          <Photos></Photos>
+          <Descs></Descs>
+        </Step1>
+      </Steps>
     </Third>
   );
 }
@@ -58,6 +110,7 @@ export const Span2 = styled.span`
   width: unset;
   white-space: normal;
   line-height: inherit;
+  margin-bottom: 80px;
 `;
 
 export const Span2_1 = styled.span`
@@ -69,4 +122,120 @@ export const Span2_1 = styled.span`
   width: unset;
   white-space: normal;
   line-height: inherit;
+`;
+
+export const Steps = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  width: 100%;
+  padding: 0px 40px;
+  max-width: 1280px;
+`;
+
+export const Step1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 36px;
+  align-items: flex-start;
+  box-pack: start;
+  justify-content: flex-start;
+`;
+
+export const Step2 = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  gap: 36px;
+  align-items: flex-start;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+`;
+
+export const Photos = styled.div`
+  position: relative;
+  width: 521px;
+  height: 400px;
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 24px;
+  box-shadow: rgba(142, 147, 148, 0.08) 16px 16px 18px 0px,
+    rgba(116, 123, 126, 0.12) 5px 5px 10px 0px;
+  flex-shrink: 0;
+  margin: 0px 82px 30px;
+`;
+
+export const Descs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+export const StepNumber = styled.div`
+  width: 54px;
+  height: 54px;
+  flex-shrink: 0;
+  border-radius: 24px;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  background: linear-gradient(
+    270deg,
+    rgb(1, 172, 20) 0%,
+    rgb(1, 172, 20) 50%,
+    rgb(0, 164, 16) 50%,
+    rgb(0, 164, 16) 100%
+  );
+`;
+
+export const StepNumberSpan = styled.span`
+  font-family: inherit;
+  font-size: 32px;
+  font-weight: 600;
+  color: white;
+  text-align: inherit;
+  width: 17px;
+  white-space: normal;
+  line-height: inherit;
+`;
+
+export const Desc1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const Desc1_1 = styled.span`
+  font-family: Maplestory;
+  font-size: 20px;
+  font-weight: 300;
+  color: var(--point_01);
+  text-align: inherit;
+  width: unset;
+  white-space: normal;
+  line-height: inherit;
+`;
+
+export const Desc1_2 = styled.span`
+  font-family: Maplestory;
+  font-size: 38px;
+  font-weight: 300;
+  color: inherit;
+  text-align: inherit;
+  width: unset;
+  white-space: normal;
+  line-height: inherit;
+`;
+
+export const Desc2 = styled.span`
+  font-family: SUIT;
+  font-size: 18px;
+  font-weight: 500;
+  color: rgb(117, 126, 133);
+  text-align: inherit;
+  width: unset;
+  white-space: break-spaces;
+  line-height: 1.6;
 `;
