bc473d4cc61ab7a401d85d4d085ff0f7e033b114
kimjihye058 <s2321@e-mirim.hs.kr>
2025-09-03 12:14:22 +0900

    #8 Card부분 구현

--- holingclone/src/components/Section5.tsx
+++ holingclone/src/components/Section5.tsx
@@ -10,47 +10,84 @@ function Section5() {
         <CardDiv>
           <ConDiv>
             <ImgDiv>
-              <img src="/assets/images/parents/card1.png" alt="" />
-              <img src="/assets/images/parents/quote.svg" alt="" />
+              <img
+                src="/assets/images/parents/card1.png"
+                alt=""
+                style={{ width: "100%" }}
+              />
+              <img
+                src="/assets/images/parents/quote.svg"
+                alt=""
+                width="50"
+                style={{ position: "relative", top: "-12px" }}
+              />
             </ImgDiv>
-            <ConSpan1></ConSpan1>
+            <TitleSpan>강의 덕분에 부모 도움이 필요없어요</TitleSpan>
             <HumanDiv>
-              <NameSpan></NameSpan>
-              <ParentDiv></ParentDiv>
-              <ParentSpan></ParentSpan>
+              <NameSpan>박O영</NameSpan>
+              <ParentDiv />
+              <ParentSpan>초등학교 5학년 학부모</ParentSpan>
             </HumanDiv>
           </ConDiv>
-          <ConSpan2></ConSpan2>
+          <ConSpan>
+            아이가 어려워하는 문제가 있으면 부모에게 물어보지 않아도 강의
+            영상으로 다시 한 번 개념을 확인할 수 있어서 좋아요.
+          </ConSpan>
         </CardDiv>
         <CardDiv>
           <ConDiv>
             <ImgDiv>
-              <img src="/assets/images/parents/card2.png" alt="" />
-              <img src="/assets/images/parents/quote.svg" alt="" />
+              <img
+                src="/assets/images/parents/card2.png"
+                alt=""
+                style={{ width: "100%" }}
+              />{" "}
+              <img
+                src="/assets/images/parents/quote.svg"
+                alt=""
+                width="50"
+                style={{ position: "relative", top: "-12px" }}
+              />{" "}
             </ImgDiv>
-            <ConSpan1></ConSpan1>
+            <TitleSpan>게임처럼 공부하니<br />지루해하지 않아요</TitleSpan>
             <HumanDiv>
-              <NameSpan></NameSpan>
-              <ParentDiv></ParentDiv>
-              <ParentSpan></ParentSpan>
+              <NameSpan>박O영</NameSpan>
+              <ParentDiv />
+              <ParentSpan>초등학교 2학년 학부모</ParentSpan>
             </HumanDiv>
           </ConDiv>
-          <ConSpan2></ConSpan2>
+          <ConSpan>
+            예습과 복습을 모두 할 수 있다는 게 장점이에요. 지루하지 않게
+            게임처럼 즐기면서 공부할 수 있는 학습 구성도 마음에 들어요.
+          </ConSpan>
         </CardDiv>
         <CardDiv>
           <ConDiv>
             <ImgDiv>
-              <img src="/assets/images/parents/card3.png" alt="" />
-              <img src="/assets/images/parents/quote.svg" alt="" />
+              <img
+                src="/assets/images/parents/card3.png"
+                alt=""
+                style={{ width: "100%" }}
+              />{" "}
+              <img
+                src="/assets/images/parents/quote.svg"
+                alt=""
+                width="50"
+                style={{ position: "relative", top: "-12px" }}
+              />{" "}
             </ImgDiv>
-            <ConSpan1></ConSpan1>
+            <TitleSpan>아이의 실력을 바로<br />확인할 수 있어요</TitleSpan>
             <HumanDiv>
-              <NameSpan></NameSpan>
-              <ParentDiv></ParentDiv>
-              <ParentSpan></ParentSpan>
+              <NameSpan>김O영</NameSpan>
+              <ParentDiv />
+              <ParentSpan>초등학교 3학년 학부모</ParentSpan>
             </HumanDiv>
           </ConDiv>
-          <ConSpan2></ConSpan2>
+          <ConSpan>
+            주간 학습 리포트로 지난주에 어떻게 공부했는지 알 수 있어요. 또, 학습
+            현황을 보면서 아이의 문제 푸는 능력을 객관적으로 확인할 수 있어서
+            좋아요.
+          </ConSpan>
         </CardDiv>
       </Cards>
     </Fifth>
@@ -87,25 +124,107 @@ export const Span2 = styled.span`
   width: unset;
   white-space: normal;
   line-height: inherit;
-  -webkit-text-stroke: 0.3px rgb(0, 171, 21);
 `;
 
-export const Cards = styled.div``;
+export const Cards = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 36px;
+  box-align: center;
+  align-items: center;
+  box-pack: center;
+  justify-content: center;
+`;
 
-export const CardDiv = styled.div``;
+export const CardDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  box-align: center;
+  align-items: center;
+  padding: 30px 0px;
+  width: 376px;
+  border-radius: 30px;
+  background: rgb(255, 255, 255);
+  box-shadow: rgba(85, 86, 86, 0.12) 4px 4px 20px 0px;
+`;
 
-export const ConDiv = styled.div``;
+export const ConDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  width: 296px;
+  box-align: center;
+  align-items: center;
+`;
 
-export const ImgDiv = styled.div``;
+export const ImgDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-align: center;
+  align-items: center;
+`;
 
-export const ConSpan1 = styled.span``;
+export const TitleSpan = styled.span`
+  font-family: Maplestory;
+  font-size: 30px;
+  font-weight: 300;
+  color: inherit;
+  text-align: center;
+  width: unset;
+  white-space: break-spaces;
+  line-height: 1.4;
+`;
 
-export const HumanDiv = styled.div``;
+export const HumanDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 18px;
+  font-weight: 500;
+  font-family: SUIT;
+  color: rgb(117, 126, 133);
+  gap: 10px;
+  box-align: center;
+  align-items: center;
+`;
 
-export const NameSpan = styled.span``;
+export const NameSpan = styled.span`
+  font-family: inherit;
+  font-size: 16px;
+  font-weight: 500;
+  color: inherit;
+  text-align: inherit;
+  width: unset;
+  white-space: normal;
+  line-height: inherit;
+`;
 
-export const ParentDiv = styled.div``;
+export const ParentDiv = styled.div`
+  flex-shrink: 0;
+  width: 1px;
+  height: 14px;
+  background-color: rgb(217, 221, 224);
+`;
 
-export const ParentSpan = styled.div``;
+export const ParentSpan = styled.div`
+  font-family: inherit;
+  font-size: 16px;
+  font-weight: 500;
+  color: inherit;
+  text-align: inherit;
+  width: unset;
+  white-space: normal;
+  line-height: inherit;
+`;
 
-export const ConSpan2 = styled.span``;
+export const ConSpan = styled.span`
+  font-family: SUIT;
+  font-size: 18px;
+  font-weight: 500;
+  color: inherit;
+  text-align: center;
+  white-space: normal;
+  line-height: 1.5;
+    width: 296px;
+
+`;
